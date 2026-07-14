a556b3f4c6f2154f061aede7032726327a7866f5
bold6609 <lucianomartellisalazar@colegionahuelcura.cl>
2026-07-13 20:46:04 -0400

    Primer proyecto Bold Motors

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    alert("Gracias por contactar a Bold Motors. Pronto un asesor se comunicará contigo.");
+
+});
